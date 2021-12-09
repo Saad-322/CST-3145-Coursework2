66340d188b932928406fec7ff57da6e46b1a9212
Saad-322 <saady6nov@gmail.com>
2021-12-09 14:34:38 +0000

    display users and lessons from textfile

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-const express = require('express') //Requires the Express module
-const fs = require('fs')
+const express = require('express')
 const path = require('path')
-const http = require('http')
-const { url } = require('inspector')
-
-//calls the express function to stat a new Express application
+const fs = require('fs')
 const app = express()
 
-app.use (function(request, response, next){//middleware
-    console.log("server running for "+request.url)
-    next()
-})
-app.use(function(request,response,next){
-    var filePath = path.join(__dirname,"static",request.url);
+app.get('/lessons',function(request,response,next){
+    var filePath = path.join(__dirname,"static",'lessons.txt')
     fs.stat(filePath,function(err,fileInfo){
         if(err){
             next()
             return
         }
-        if(fileInfo.isFile()) response.sendFile(filePath)
+        else if(fileInfo.isFile()) response.sendFile(filePath)
     })
 })
-app.use(function(request,response){
-    response.status(404)
-    response.end("file not found BRO")
+
+app.get('/user',function(request,response,next){
+    var filePath = path.join(__dirname,"static",'users.txt')
+    fs.stat(filePath,function(err,fileInfo){
+        if(err){
+            next()
+            return
+        }
+        else if(fileInfo.isFile()){
+            response.sendFile(filePath)
+        }
+    })
 })
 
-app.listen(3000,function(){
-    console.log("Express server started.")
-}) //Start the server
+app.use(function(request,response){
+    response.send("Error: File not found")
+})
+app.listen(3000)
