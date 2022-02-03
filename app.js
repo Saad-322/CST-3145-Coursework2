98567935ca39056d1d75e58aa23d6317b1d2827d
Saad-322 <saady6nov@gmail.com>
2022-02-03 17:01:48 +0000

    code cleaned

--- app.js
+++ app.js
@@ -9,19 +9,10 @@ app.use(cors())
 
 const mongoClient = require('mongodb').MongoClient
 
+//connect to mongoDB
 let db
 mongoClient.connect('mongodb+srv://sa3063:1234@cluster0.mmu69.mongodb.net/lessons_store?retryWrites=true&w=majority', function(err,client){
     db = client.db('lessons_store')
-})
-
-app.param('collectionName', function(req,res,next,collectionName){
-    req.collection = db.collection(collectionName)
-    return next()
-})
-
-//root
-app.get('/',function(req,res){
-    res.send("/collection/lessons to view all lessons")
 })
 
 //logger
@@ -29,6 +20,17 @@ app.use(function(req,res,next){
     console.log("Request ID: "+req.url)
     console.log("Request Date: "+ new Date())
     next()
+})
+
+//identify collection name for all requests
+app.param('collectionName', function(req,res,next,collectionName){
+    req.collection = db.collection(collectionName)
+    return next()
+})
+
+//root url
+app.get('/',function(req,res){
+    res.send("/collection/lessons to view all lessons")
 })
 
 //serve static image files
@@ -91,42 +93,7 @@ app.post('/collection/:collectionName',function(req,res,next){
     })
 })
 
-let searchValue
-// app.post('/search',function(req,res){
-//     searchValue = JSON.stringify(req.body.value)
-//     // change
-//     req.collection = db.collection('lessons')
-//     req.collection.find({}).toArray(function(err,results,next){
-//         if (err){
-//             return next(err)
-//         }
-//         else{
-//             for (let i = 0; i < results.length; i++) {
-//                 let Subject = results[i].Subject.toLowerCase();
-//                 let Location = results[i].Location.toLowerCase();
-//                 if (!(Subject.includes(this.searchValue.toLowerCase()) || Location.toLowerCase().includes(this.searchValue.toLowerCase()))) {
-//                     req.collection.updateOne(
-//                         {lessonID: results[i].lessonID},
-//                         {$set: JSON.stringify({'Visible':false})},
-//                         {safe: true, multi:false}
-//                     )
-//                 }
-//                 else{
-//                     req.collection.updateOne(
-//                         {lessonID: results[i].lessonID},
-//                         {$set: JSON.stringify({'Visible':true})},
-//                         {safe: true, multi:false}
-//                     )
-//                 }
-//             }
-//         }
-//     })
-
-// })
-
-const objectID = require('mongodb').ObjectId
-
-//updates number of spaces after order is submitted
+//updates number of spaces using unique lessonID after order is submitted
 app.put('/collection/:collectionName/:lessonID',function(req,res,next){
     req.collection.updateOne(
         {lessonID: req.params.lessonID},
@@ -147,27 +114,11 @@ app.put('/collection/:collectionName/:lessonID',function(req,res,next){
         }
     )
 })
-app.delete('/collection/:collectionName/:id',function(req,res,next){
-    req.collection.deleteOne(
-        {_id: new objectID(req.params.id)},
-        {$set: req.body},
-        {safe: true, multi:false},
-        function(err,result){
-            if (err){
-                return next(err)
-            }
-            else{
-                res.send(result)
-            }
-        }
-    )
-})
 
 //Error handling
 app.use(function(req,res){
-    console.log("Request ID: "+req.url)
-    console.log("Request Date: "+ new Date())
     res.send("error")
 })
+
 const port = process.env.PORT || 3000
 app.listen(port)
